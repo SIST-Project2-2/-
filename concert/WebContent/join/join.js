7e8f3fccc932ba3455b8ef321f8e93ad3b5218b8
sayskpays <76785132+sayskpays@users.noreply.github.com>
2021-05-25 14:49:50 +0900

    회원가입UI
    JSP파일 하나로 통합
    수정 =유효성 검사X, JSP파일 변경X

--- concert/WebContent/join/join.js
+++ concert/WebContent/join/join.js
@@ -1,28 +1,5 @@
 'use strict';
-<!--  -->
-/* 아이디 창 : 다음 버튼 누르면 패스워드 입력 창으로 이동*/
-function passPage()  {
-        window.location.href = "http://localhost:9000/concert/join/join_pass.html";
-  }
-/* 패스워드 창 : 다음 버튼 누르면 이름,주소 등록 창으로 이동*/
-function namePage(){
-        window.location.href = "http://localhost:9000/concert/join/join_name.html";
-}
-/*이름, 주소 입력창 누르면 휴대폰번호 등록창으로 이동 */
-function hpPage(){
-        window.location.href = "http://localhost:9000/concert/join/join_hp.html";
-}
-/*다음 버튼 누르면 생년월일 등록창으로 이동*/
-function birthPage(){
-        window.location.href = "http://localhost:9000/concert/join/join_birth.html";
-}
-/*다음 버튼 누르면 프로필 등록창으로 이동*/
-function profilePage(){
-        window.location.href = "http://localhost:9000/concert/join/join_profile.html"
-}
-function home(){
-    alert("회원가입이 완료되었습니다.");
-}
+
 
 /*패스워드 확인*/
 function passCheck(){
@@ -41,8 +18,6 @@ function passCheck(){
     }
 }
 
-// /*아이디 중복확인*/ 
-// function idCheck(){
-    
-// }
-
+function home(){
+    alert("회원가입이 완료되었습니다.");
+}
