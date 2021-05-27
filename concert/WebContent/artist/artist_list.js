bc0d111595b5298c6b1225da8d52576a3f32c896
sayskpays <76785132+sayskpays@users.noreply.github.com>
2021-05-27 18:45:33 +0900

    아티스트 목록UI
    IU 페이지 완료
    
    추가할 사항: 나머지 네 명 아티스트 페이지

---
+++ concert/WebContent/artist/artist_list.js
@@ -0,0 +1,43 @@
+/* 아이유 노래 실행/끄기 */ 
+function play1() {
+    let audio1 = document.getElementById("audio1");
+    audio1.play();
+}
+function pause1() {
+    let audio1 = document.getElementById("audio1");
+    audio1.pause();
+}
+
+function play2() {
+    let audio2 = document.getElementById("audio2");
+    audio2.play();
+}
+function pause2() {
+    let audio2 = document.getElementById("audio2");
+    audio2.pause();
+}
+
+function play3(){
+    let audio3 = document.getElementById("audio3");
+    audio3.play();
+}
+function pause3(){
+    let audio3 = document.getElementById("audio3");
+    audio3.pause();
+}
+function play4(){
+    let audio4 = document.getElementById("audio4");
+    audio4.play();
+}
+function pause4(){
+    let audio4 = document.getElementById("audio4");
+    audio4.pause();
+}
+function play5(){
+    let audio5 = document.getElementById("audio5");
+    audio5.play();
+}
+function pause5(){
+    let audio5 = document.getElementById("audio5");
+    audio5.pause();
+}
