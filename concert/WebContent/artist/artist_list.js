d3e550c4f67436a28b5cbfd0d6086c6fdb1e39ff
sayskpays <76785132+sayskpays@users.noreply.github.com>
2021-06-01 11:04:13 +0900

    아티스트 목록 노래재생

--- concert/WebContent/artist/artist_list.js
+++ concert/WebContent/artist/artist_list.js
@@ -1,3 +1,14 @@
+function play(name) {
+    let audio = document.getElementById(name);
+        audio.play();
+}
+
+function pause(name) {
+    let audio = document.getElementById(name);
+    audio.pause();
+}
+
+/* 아이유 노래 실행/끄기 */ 
 function iu_play1() {
     let audio1 = document.getElementById("iu_audio1");
         audio1.play();
@@ -124,6 +135,215 @@ function jan_play5(){
 }
 function jan_pause5(){
     let audio5 = document.getElementById("jan_audio5");
+}
+
+function iu_play2() {
+    let audio2 = document.getElementById("iu_audio2");
+    audio2.play();
+}
+function iu_pause2() {
+    let audio2 = document.getElementById("iu_audio2");
+    audio2.pause();
+}
+
+function iu_play3(){
+    let audio3 = document.getElementById("iu_audio3");
+    audio3.play();
+}
+function iu_pause3(){
+    let audio3 = document.getElementById("iu_audio3");
+    audio3.pause();
+}
+function iu_play4(){
+    let audio4 = document.getElementById("iu_audio4");
+    audio4.play();
+}
+function iu_pause4(){
+    let audio4 = document.getElementById("iu_audio4");
+    audio4.pause();
+}
+function iu_play5(){
+    let audio5 = document.getElementById("iu_audio5");
+    audio5.play();
+}
+function iu_pause5(){
+    let audio5 = document.getElementById("iu_audio5");
+    audio5.pause();
+}
+
+/*장범준 노래 실행/ 끄기  */
+function jang_play1() {
+    let audio1 = document.getElementById("jang_audio1");
+        audio1.play();
+}
+function jang_pause1() {
+    let audio1 = document.getElementById("jang_audio1");
+    audio1.pause();
+}
+
+function jang_play2() {
+    let audio2 = document.getElementById("jang_audio2");
+    audio2.play();
+}
+function jang_pause2() {
+    let audio2 = document.getElementById("jang_audio2");
+    audio2.pause();
+}
+
+function jang_play3(){
+    let audio3 = document.getElementById("jang_audio3");
+    audio3.play();
+}
+function jang_pause3(){
+    let audio3 = document.getElementById("jang_audio3");
+    audio3.pause();
+}
+function jang_play4(){
+    let audio4 = document.getElementById("jang_audio4");
+    audio4.play();
+}
+function jang_pause4(){
+    let audio4 = document.getElementById("jang_audio4");
+    audio4.pause();
+}
+function jang_play5(){
+    let audio5 = document.getElementById("jang_audio5");
+    audio5.play();
+}
+function jang_pause5(){
+    let audio5 = document.getElementById("jang_audio5");
+    audio5.pause();
+}
+
+/* 잔나비 노래 실행/끄기  */
+function jan_play1() {
+    let audio1 = document.getElementById("jan_audio1");
+        audio1.play();
+}
+function jan_pause1() {
+    let audio1 = document.getElementById("jan_audio1");
+    audio1.pause();
+}
+
+function jan_play2() {
+    let audio2 = document.getElementById("jan_audio2");
+    audio2.play();
+}
+function jan_pause2() {
+    let audio2 = document.getElementById("jan_audio2");
+    audio2.pause();
+}
+
+function jan_play3(){
+    let audio3 = document.getElementById("jan_audio3");
+    audio3.play();
+}
+function jan_pause3(){
+    let audio3 = document.getElementById("jan_audio3");
+    audio3.pause();
+}
+function jan_play4(){
+    let audio4 = document.getElementById("jan_audio4");
+    audio4.play();
+}
+function jan_pause4(){
+    let audio4 = document.getElementById("jan_audio4");
+    audio4.pause();
+}
+function jan_play5(){
+    let audio5 = document.getElementById("jan_audio5");
+    audio5.play();
+}
+function jan_pause5(){
+    let audio5 = document.getElementById("jan_audio5");
+    audio5.pause();
+}
+
+/*현아 노래 실행/끄기 */
+function hyun_play1() {
+    let audio1 = document.getElementById("hyun_audio1");
+        audio1.play();
+}
+function hyun_pause1() {
+    let audio1 = document.getElementById("hyun_audio1");
+    audio1.pause();
+}
+
+function hyun_play2() {
+    let audio2 = document.getElementById("hyun_audio2");
+    audio2.play();
+}
+function hyun_pause2() {
+    let audio2 = document.getElementById("hyun_audio2");
+    audio2.pause();
+}
+
+function hyun_play3(){
+    let audio3 = document.getElementById("hyun_audio3");
+    audio3.play();
+}
+function hyun_pause3(){
+    let audio3 = document.getElementById("hyun_audio3");
+    audio3.pause();
+}
+function hyun_play4(){
+    let audio4 = document.getElementById("hyun_audio4");
+    audio4.play();
+}
+function hyun_pause4(){
+    let audio4 = document.getElementById("hyun_audio4");
+    audio4.pause();
+}
+function hyun_play5(){
+    let audio5 = document.getElementById("hyun_audio5");
+    audio5.play();
+}
+function hyun_pause5(){
+    let audio5 = document.getElementById("hyun_audio5");
+    audio5.pause();
+}
+
+/* 십센치 노래 실행/끄기 */
+function cm_play1() {
+    let audio1 = document.getElementById("cm_audio1");
+        audio1.play();
+}
+function cm_pause1() {
+    let audio1 = document.getElementById("cm_audio1");
+    audio1.pause();
+}
+
+function cm_play2() {
+    let audio2 = document.getElementById("cm_audio2");
+    audio2.play();
+}
+function cm_pause2() {
+    let audio2 = document.getElementById("cm_audio2");
+    audio2.pause();
+}
+
+function cm_play3(){
+    let audio3 = document.getElementById("cm_audio3");
+    audio3.play();
+}
+function cm_pause3(){
+    let audio3 = document.getElementById("cm_audio3");
+    audio3.pause();
+}
+function cm_play4(){
+    let audio4 = document.getElementById("cm_audio4");
+    audio4.play();
+}
+function cm_pause4(){
+    let audio4 = document.getElementById("cm_audio4");
+    audio4.pause();
+}
+function cm_play5(){
+    let audio5 = document.getElementById("cm_audio5");
+    audio5.play();
+}
+function cm_pause5(){
+    let audio5 = document.getElementById("cm_audio5");
     audio5.pause();
 }
 
