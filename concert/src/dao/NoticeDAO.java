a9bb0baec28039df4191077e2c7584dc08181501
Judongsung <57513715+Judongsung@users.noreply.github.com>
2021-06-03 01:11:45 +0900

    getPageProcess() 함수 이동
    NoticeDAO.java
    - getPageProcess() 함수를 Commons 클래스에 static 함수로 옮겼습니다. 그에 따라 일부 코드 수정
    
    Commons.java
    - NoticeDAO 클래스의 getPageProcess() 함수를 여기로 옮겼습니다.
    - getPageProcess() 함수에서 매개변수로 post_per_page를 추가로 받도록 하였습니다.

--- concert/src/dao/NoticeDAO.java
+++ concert/src/dao/NoticeDAO.java
@@ -226,43 +226,43 @@ public class NoticeDAO extends DAO {
 	}
 	
 	// PageVO를 설정하는 함수
-	public PageVO getPageProcess(int count, int nowPage) {
-		PageVO vo = new PageVO();
-		int total_page = count / notice_per_page; // 전체 페이지 수
-		if(count % notice_per_page != 0) {
-			total_page ++;
-		} 
-		
-		vo.setTotal(total_page);
-		
-		// 현 페이지가 1인 경우 이전 버튼 비활성화
-		if(nowPage == 1) { 
-			vo.setPrev(false);
-		}else {
-			vo.setPrev(true);
-		}
-		
-		// 현 페이지가 마지막 페이지인 경우 다음 버튼 비활성화
-		if(nowPage == total_page || total_page == 0) { 
-			vo.setNext(false);
-		}else {
-			vo.setNext(true);
-		}
-		
-		// 보여지는 페이지 중 시작 페이지와 끝 페이지
-		if(nowPage <= 5) { // 현 페이지가 5페이지 이내일 경우
-			vo.setStart(1);
-			vo.setEnd(total_page);
-		}else if(total_page - nowPage <= 2) { // 현 페이지가 뒤에서 2번째 이내일 경우
-			vo.setStart(total_page - 4);
-			vo.setEnd(total_page);
-		}else { // 그 외의 경우
-			vo.setStart(nowPage - 2);
-			vo.setEnd(nowPage + 2);
-		}
-		
-		return vo;
-	}
+//	public PageVO getPageProcess(int count, int nowPage) {
+//		PageVO vo = new PageVO();
+//		int total_page = count / notice_per_page; // 전체 페이지 수
+//		if(count % notice_per_page != 0) {
+//			total_page ++;
+//		} 
+//		
+//		vo.setTotal(total_page);
+//		
+//		// 현 페이지가 1인 경우 이전 버튼 비활성화
+//		if(nowPage == 1) { 
+//			vo.setPrev(false);
+//		}else {
+//			vo.setPrev(true);
+//		}
+//		
+//		// 현 페이지가 마지막 페이지인 경우 다음 버튼 비활성화
+//		if(nowPage == total_page || total_page == 0) { 
+//			vo.setNext(false);
+//		}else {
+//			vo.setNext(true);
+//		}
+//		
+//		// 보여지는 페이지 중 시작 페이지와 끝 페이지
+//		if(nowPage <= 5) { // 현 페이지가 5페이지 이내일 경우
+//			vo.setStart(1);
+//			vo.setEnd(total_page);
+//		}else if(total_page - nowPage <= 2) { // 현 페이지가 뒤에서 2번째 이내일 경우
+//			vo.setStart(total_page - 4);
+//			vo.setEnd(total_page);
+//		}else { // 그 외의 경우
+//			vo.setStart(nowPage - 2);
+//			vo.setEnd(nowPage + 2);
+//		}
+//		
+//		return vo;
+//	}
 	
 	// 페이지 구하기 - 기본
 	public PageVO getPageInfo(int nowPage) {
@@ -280,7 +280,7 @@ public class NoticeDAO extends DAO {
 			e.printStackTrace();
 		}
 		
-		return getPageProcess(count, nowPage);
+		return Commons.getPageProcess(count, nowPage, notice_per_page);
 	}
 	
 	// 페이지 구하기 - 가수별
@@ -302,7 +302,7 @@ public class NoticeDAO extends DAO {
 			e.printStackTrace();
 		}
 		
-		return getPageProcess(count, nowPage);
+		return Commons.getPageProcess(count, nowPage, notice_per_page);
 	}
 	
 	// 페이지 구하기 - 검색
@@ -334,7 +334,7 @@ public class NoticeDAO extends DAO {
 			e.printStackTrace();
 		}
 		
-		return getPageProcess(count, nowPage);
+		return Commons.getPageProcess(count, nowPage, notice_per_page);
 	}
 	
 	// 페이지 구하기 - 아티스트별 + 검색
@@ -368,6 +368,6 @@ public class NoticeDAO extends DAO {
 			e.printStackTrace();
 		}
 		
-		return getPageProcess(count, nowPage);
+		return Commons.getPageProcess(count, nowPage, notice_per_page);
 	}
 }
