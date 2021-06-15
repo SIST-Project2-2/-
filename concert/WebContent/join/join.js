103626adbd41d7c39b4f17d4107bc8a40914ba48
sayskpays <76785132+sayskpays@users.noreply.github.com>
2021-06-15 16:30:52 +0900

    회원가입 유효성검사 추가
    - join.js
    성별 카운트(유효성 검사), 주소 API코드 추가
    
    - join.jsp
    아이디 입력창에 버튼 삭제(바로 중복검사)
    데이터 입력시 다음 버튼 클릭 가능하도록 수정
    이름 입력창에 문자만 입력 가능하도록 수정
    생년월일 창에 숫자만 기입 가능

--- concert/WebContent/join/join.js
+++ concert/WebContent/join/join.js
@@ -6,19 +6,59 @@ function passCheck(){
         let pw = document.getElementById("pw").value;
         let cpass = document.getElementById("cpass").value;
         let msg = document.getElementById("msg");
-
+		
+		var btn = document.getElementById("passBtn");
+			
         if(pw!="" && cpass!=""){
 			
         if(pw === cpass){
             msg.innerHTML="패스워드가 일치합니다"
+			msg.style.color="green";
+			btn.disabled = false;
         }else{
             msg.innerHTML="패스워드가 일치하지 않습니다"
+			btn.disabled = 'disabled';
+			msg.style.color = "red";
             cpass.value="";
             cpass.focus;
+ 		 	
         }
     }
 }
 
 function home(){
     alert("회원가입이 완료되었습니다.");
+}
+
+
+//성별 카운트 체크
+function check_count(gender) {
+	var gender_list = $("input[name='gender']:checked");
+
+	var count = 0;
+	for (var i = 0; i < gender_list.length; i++) {
+		if (gender_list[i].checked) {
+			count++;
+		}
+	}
+	return count;
+}
+
+//주소검색 API
+	function goPopup() {
+		// 주소검색을 수행할 팝업 페이지를 호출
+		// 호출된 페이지(jusopopup.jsp)에서 실제 주소검색URL(https://www.juso.go.kr/addrlink/addrLinkUrl.do)를 호출
+		var pop = window.open("/concert/join/jusoPopup.jsp", "pop",
+				"width=570,height=420, scrollbars=yes, resizable=yes");
+	}
+
+	function jusoCallBack(roadAddrPart1, addrDetail) {
+		// 팝업페이지에서 주소입력한 정보를 받아서, 현 페이지에 정보를 등록
+
+		let addr = document.querySelector("#addr");
+		let daddr = document.querySelector("#daddr");
+
+		//주소,상새주소 input칸에 주소검색API에서 찾은 데이터 value 값 넣기
+		addr.value = roadAddrPart1;
+		daddr.value = addrDetail;
 }
